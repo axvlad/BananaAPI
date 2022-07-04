f74fa1891399d63296d8d03ef1a8dd75eec1e018
Vlad Burma <100062601+axvlad@users.noreply.github.com>
2022-07-04 11:53:29 +0300

    Add files via upload

---
+++ BananaAPI/BananaAPI/Banana.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BananaAPI
+{
+    public class Banana
+    {
+        public string name { get; set; }
+
+        public string weight { get; set; }
+
+        public int amount { get; set; }
+
+        public Banana()
+        {
+            name = "Yellow banana";
+            weight = "3 kilogram";
+            amount = 15;
+        }
+
+        public override string ToString()
+        {
+            return $"name {name} weight {weight} amount {amount}";
+        }
+
+    }
+}
---
+++ BananaAPI/BananaAPI/Controllers/BananaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BananaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class BananaController : Controller
+    {
+        public Banana PrintInfo()
+        {
+            Banana yellow = new Banana();
+            return yellow;
+        }
+
+        [HttpGet]
+        public Banana Get()
+        {
+            return PrintInfo();
+        }
+    }
+}
